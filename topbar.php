6cbf758e5d7cb4c6b2dc74ec8ebb68042c217937
clementhelsens <clement.helsens@cern.ch>
2018-01-18 14:09:38 +0100

    menu.php

---
+++ topbar.php
@@ -0,0 +1,29 @@
+<div class="topnav">
+  <a class="active" href="http://fcc-physics-events.web.cern.ch/fcc-physics-events/">Home</a>
+  <a href="#about">About</a>
+  <a href="secure_email_form.php">Contact</a>
+
+<ul>
+  <li><b href="#">Physics</b>
+    <ul>
+      <li><a href="LHEevents.php">LHE 100TeV</a></li>
+      <li><a href="Delphesevents_fcc_v01.php">FCC v01</a></li>
+      <li><a href="Delphesevents_fcc_v02.php">FCC v02</a></li>
+      <li><a href="Delphesevents_cms.php">CMS</a></li>
+    </ul>
+  </li>
+
+  <li><b href="#">Full Simulation</b>
+    <ul>
+      <li><a href="FCCsim_v01.php">FCCSim v01</a></li>
+
+    </ul>
+  </li>
+</ul>
+
+</div>
+
+
+
+
+
