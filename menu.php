8b6ce21ddfd2ace0b2c649b1712a6abb9a792b0c
clementhelsens <clement.helsens@cern.ch>
2018-01-18 14:10:00 +0100

    initial import

---
+++ menu.php
@@ -0,0 +1,14 @@
+<select name="selection" onchange="document.location.href = this.options[this.selectedIndex].value;">
+    <option selected="selected">Menu Physics</option>
+    <option value="LHEevents.php">Les Houches Events</option>
+    <option value="Delphesevents_fcc_v01.php">Delphes events fcc_v01</option>
+    <option value="Delphesevents_fcc_v02.php">Delphes events fcc_v02</option>
+    <option value="Delphesevents_cms.php">Delphes events CMS</option>
+    <option value="secure_email_form.php">Contact</option>
+</select>
+
+<select name="selection" onchange="document.location.href = this.options[this.selectedIndex].value;">
+    <option selected="selected">Menu Simulation</option>
+    <option value="FCCsim_v01.php">FCCSim v01</option>
+    <option value="secure_email_form.php">Contact</option>
+</select>
