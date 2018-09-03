5ce9474998b0e03478cd718bc24f11fa3c35d1d8
clementhelsens <clement.helsens@cern.ch>
2018-09-03 14:49:06 +0200

    Update search.php

--- search.php
+++ search.php
@@ -13,10 +13,14 @@ function search() {
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[1];
     if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+       
+      foreach ($filters as &$value) {
+        
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
+      }
       }
     }
   }
