a05f1724dcfec08bcce297e8586ee1ae6ad58fb6
clementhelsens <clement.helsens@cern.ch>
2021-04-23 12:16:17 +0200

    rm FS stuff

--- topbar.php
+++ topbar.php
@@ -13,18 +13,8 @@
     </ul>
   </li>
 
-
-  <li><b href="#">27TeV HE-LHC Physics</b>
-   <ul>
-      <li><a href="LHEevents_helhc.php">Les Houches </a></li>
-      <li><a href="Delphesevents_helhc_v01.php">HE-LHC v01</a></li>
-   </ul>
-  </li>
-
-
   <li><b href="#">Full Simulation FCChh</b>
     <ul>
-      <li><a href="FCCsim_v02_pre.php">FCCSim v02_pre</a></li>
       <li><a href="FCCsim_v03.php">FCCSim v03</a></li>
       <li><a href="FCCsim_v03_ecal.php">FCCSim v03 ecal</a></li>
     </ul>
@@ -45,10 +35,8 @@
   <li><b href="#">Stat</b>
     <ul>
       <li><a href="data/FCChh/statlhe.html">LHE 100TeV</a></li>
-      <li><a href="data/HELHC/statlhe.html">LHE 27TeV</a></li>
       <li><a href="data/FCChh/statdelphesfcc_v02.html">FCC v02</a></li>
       <li><a href="data/FCChh/statdelphesfcc_v03.html">FCC v03</a></li>
-      <li><a href="data/HELHC/statdelpheshelhc_v01.html">HE-LHC v01</a></li>
       <li><a href="data/FCCee/statdelphesfcc_v01.html">FCCee v01</a></li>
 
     </ul>
