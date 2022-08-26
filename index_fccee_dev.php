1f557a93421c21f6c70add84944c06cf8ef7b369
clementhelsens <clement.helsens@cern.ch>
2022-08-26 11:41:24 +0200

    add more files

---
+++ index_fccee_dev.php
@@ -0,0 +1,34 @@
+
+<?
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+$debug=(isset($_GET["debug"])?$_GET["debug"]:false);
+if(!isset($gobase)){$gobase="";}
+?>
+
+<html>
+<head>
+
+<style>
+<?php include 'style/main.css'; ?>
+</style>
+</head>
+
+
+<?php include 'topbar_fccee_dev.php'; ?>
+
+
+<body>
+
+
+</body>
+
+<p>FCC simulation database</p>
+
+<footer>
+<p>Created by Clement Helsens clement.helsens@cern.ch</p>
+</footer>   
+
+   </html>	
+   
+
---
+++ topbar_fccee_pre_fall2022_training.php
@@ -0,0 +1,19 @@
+<div class="topnav">
+  <a class="active" href="http://fcc-physics-events.web.cern.ch/fcc-physics-events/">Home</a>
+  <a href="secure_email_form.php">Contact</a>
+  <a href="index_fccee.php">FCC-ee</a>
+  <a href="Delphesevents_pre_fall2022_training_IDEA.php">IDEA</a>
+<ul>
+  <li><b href="#">Stat</b>
+    <ul>
+      <li><a href="data/FCCee/statdelphespre_fall2022_training_IDEA.html">   pre_fall2022 IDEA</a></li>
+    </ul>
+  </li>
+</ul>
+
+</div>
+
+
+
+
+
