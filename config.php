f510aed8d990668830c7fd738b0b502c9fefdea2
Juraj Smiesko <34742917+kjvbrt@users.noreply.github.com>
2024-05-22 10:45:37 +0200

    Setting EOS website configuration (#14)

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
 // define('BASE_URL', 'http://localhost:8000');
-define('BASE_URL', 'https://fcc-physics-events.web.cern.ch/fcc-physics-events');
+// define('BASE_URL', 'https://fcc-physics-events.web.cern.ch/fcc-physics-events');
+define('BASE_URL', 'https://fcc-physics-events.webtest.cern.ch');
 // define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
-define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/fcc-physics-events');
+// define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/fcc-physics-events');
+define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
 
 $key4hepStacks = array(
   'winter2023' => '/cvmfs/sw.hsf.org/spackages6/key4hep-stack/2022-12-23/x86_64-centos7-gcc11.2.0-opt/ll3gi/setup.sh',
